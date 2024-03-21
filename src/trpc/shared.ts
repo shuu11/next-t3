3c3cb90a90e9fdc1eb85a8c311b47d07ef548e78
shuu11 <shuu1104main@gmail.com>
2024-03-21 20:40:48 +0900

    :poop:

--- src/trpc/shared.ts
+++ src/trpc/shared.ts
@@ -7,7 +7,7 @@ export const transformer = superjson
 
 function getBaseUrl() {
 	if (typeof window !== 'undefined') return ''
-	if (process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
 	return `http://localhost:${process.env.PORT ?? 3000}`
 }
 
